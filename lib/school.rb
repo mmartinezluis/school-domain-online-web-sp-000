87ae04bc7c3d710811758c0f713288d97b95f33f
Luis Martinez <luis_mmartinez@live.com>
2020-12-31 02:26:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+  def initialize(name)
+    @name= name
+    @roster= {}
+  end
+
+  def roster
+    @roster
+  end
+
+  GRADES= [ ]
+  def add_student(student_name, grade)
+    GRADES << grade
+    if @roster == {}
+      @roster[grade]= []
+      @roster[grade] << "#{student_name}"
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort do |grade, array|
+      grade <=> array
+    end
+  end
+
+end
