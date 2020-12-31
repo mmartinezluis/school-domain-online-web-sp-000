c3f60df8ccf358fcd2d0279d19e70613e9b16397
Luis Martinez <luis_mmartinez@live.com>
2020-12-31 02:28:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,7 +3,11 @@ require "pry"
 class School
   def initialize(name)
     @name= name
-    @roster= {}
+    @roster= {
+      :9 => ["fdfdf", "dfdfdf"],
+      :10 => ["dfddf", "dfdfdf"],
+  #    :11 => ["dfdf", "dfdfdf"]
+    }
   end
 
   def roster
@@ -12,8 +16,12 @@ class School
 
   GRADES= [ ]
   def add_student(student_name, grade)
-    GRADES << grade
     if @roster == {}
+      @roster[grade] = []
+      @roster[grade] << "#{student_name}"
+    elsif @roster.keys.include? (grade)
+      @roster[grade] << "#{student_name}"
+    else
       @roster[grade]= []
       @roster[grade] << "#{student_name}"
     end
